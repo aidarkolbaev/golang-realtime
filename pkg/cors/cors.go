21de56c90720ac6cb5f69fbdc918935a4e3f6bb5
Aidar Kolbaev <aidarkolbaew@gmail.com>
2020-05-21 22:59:52 +0600

    cors middleware fix

--- pkg/cors/cors.go
+++ pkg/cors/cors.go
@@ -2,6 +2,7 @@ package cors
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func Middleware(next echo.HandlerFunc) echo.HandlerFunc {
@@ -9,6 +10,9 @@ func Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Response().Header().Set("Access-Control-Allow-Headers", "Accept,Authorization,Content-Type")
+		if c.Request().Method == http.MethodOptions {
+			return c.NoContent(http.StatusNoContent)
+		}
 		return next(c)
 	}
 }
